e492bc5834123302d0078cfaa52a99d511660325
Octavio Melendres <octavio.melendres@gmail.com>
2013-07-12 14:56:12 +0200

    Optimized grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,14 +62,42 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var geturl = function(link, callback) {
+     rest.get(link).on('complete', function(response) {
+          callback(response); });
+    };
+
+var checkHtmlUrl = function(urlcontent, checksfile) {
+    $ = cheerio.load( urlcontent );
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html://link>', 'Link to index.html','' , '')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    if (program.url) {
+      geturl(program.url, function(urlcontent) { 
+            var checkJson = checkHtmlUrl(urlcontent, program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+             });
+    }
+    else {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      console.log('Non ce URL' + program.file);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+     }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
